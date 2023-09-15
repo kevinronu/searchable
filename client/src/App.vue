58046b644d191d616b2fffb1b3d535934792c5f3
Kevin Robles <kevinronu@gmail.com>
2023-09-14 23:42:58 -0500

    chore: update structure

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Footer from "./components/Footer.vue";
+import Header from "./components/Header.vue";
+</script>
 
 <template>
+  <Header />
   <router-view />
+  <Footer />
 </template>
