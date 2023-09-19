634a92f67d954511c304e630229a15cd3a4a1e29
Kevin Robles <kevinronu@gmail.com>
2023-09-19 16:38:57 -0500

    chore: add generate query

--- server/query/query.go
+++ server/query/query.go
@@ -1,5 +1,7 @@
 package query
 
+import "fmt"
+
 func (q QuerySettings) IsValid() bool {
 	if q.Sort == "" || q.Pagination.From < 0 || q.Pagination.Size < 0 {
 		return false
@@ -14,13 +16,55 @@ func (q SearchQuerySettings) IsValidSearch() bool {
 	return true
 }
 
-func GenerateDefaultQuerySettings() QuerySettings {
-	defaultQuerySettings := QuerySettings{}
-	defaultQuerySettings.Sort = "-date"
-	defaultQuerySettings.Pagination = QueryPaginationSettings{
-		From: 0,
-		Size: 20,
+func (q QuerySettings) GenerateForDocumentsPost() string {
+	const queryTemplate = `{
+    "query": {
+      "match_all": {}
+    },
+		"_source": [],
+		"sort": ["%s"],
+		"from": %d,
+    "size": %d
+	}`
+
+	return fmt.Sprintf(queryTemplate, q.Sort, q.Pagination.From, q.Pagination.Size)
+}
+
+func (q SearchQuerySettings) GenerateForSearchDocumentPost() string {
+	const queryTemplate = `{
+		"query": {
+			"bool": {
+				"must": [
+					{
+						"query_string": {
+							"query": "%s"
+						}
+					}
+				]%s
+			}
+		},
+		"_source": [],
+		"sort": ["%s"],
+		"from": %d,
+		"size": %d
+	}`
+
+	dateRangeFilter := ""
+	if q.DateRange != nil {
+		dateRangeFilter = fmt.Sprintf(`,
+		"filter": [
+			{
+				"range": {
+					"date": {
+						"gte": "%v",
+						"lte": "%v",
+						"format": "2006-01-02 15:04:05 -0700 MST",
+						"time_zone": "UTC"
+					}
+				}
+			}
+		]`, q.DateRange.From, q.DateRange.To)
 	}
 
-	return defaultQuerySettings
+	return fmt.Sprintf(queryTemplate, q.Query, dateRangeFilter, q.Sort, q.Pagination.From, q.Pagination.Size)
 }
