6cbd4d66095a6328b6141ce760e60f544b691a59
Kevin Robles <kevinronu@gmail.com>
2023-09-18 23:12:38 -0500

    feat: add toggle dark mode

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,21 @@
 <script setup lang="ts">
+import { watch } from "vue";
 import Footer from "./components/Footer.vue";
 import Header from "./components/Header.vue";
+import { useThemeStore } from "./stores/ThemeStore"; // Adjust the import path accordingly
+
+const themeStore = useThemeStore();
+
+watch(
+  () => themeStore.isDarkMode,
+  (newValue, _) => {
+    if (newValue) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }
+);
 </script>
 
 <template>
--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import SearchFolderIconVue from "./icons/SearchFolderIcon.vue";
+import { useThemeStore } from "../stores/ThemeStore"; // Adjust the import path accordingly
+
+const themeStore = useThemeStore();
 </script>
 
 <template>
-  <header class="text-center p-4 bg-stone-400 dark:bg-stone-900 mb-6">
+  <header class="text-center p-4 bg-stone-300 dark:bg-stone-800 mb-6">
     <div
       class="container m-auto flex flex-wrap gap-4 items-center justify-between"
     >
@@ -17,7 +19,17 @@ import SearchFolderIconVue from "./icons/SearchFolderIcon.vue";
           { Search<span class="text-2xl text-pink-600">able</span> }
         </h1>
       </router-link>
-      <SearchFolderIconVue class="w-12 h-12 dark:bg-pink-600 rounded" />
+      <label class="relative text-[1em] inline-block w-[3.5em] h-[2em]">
+        <input
+          type="checkbox"
+          class="peer w-0 h-0 opacity-0"
+          v-model="themeStore.isDarkMode"
+          @change="themeStore.toggleTheme"
+        />
+        <span
+          class="absolute cursor-pointer inset-0 bg-sky-500 transition duration-400 rounded-[1.875rem] before:absolute before:content-[''] before:h-[1.4em] before:w-[1.4rem] before:rounded-[1.25rem] before:left-[0.3rem] before:bottom-[0.3em] before:bg-yellow-300 before:transition before:duration-400 peer-checked:before:bg-black peer-checked:before:rounded-[3.125rem] peer-checked:before:shadow-moon peer-checked:bg-black peer-checked:before:translate-x-[1.5em]"
+        />
+      </label>
     </div>
   </header>
 </template>
