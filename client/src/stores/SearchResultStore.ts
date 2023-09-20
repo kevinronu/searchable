c3d772e1b98e1335a5e3e241c8ca562e6e2fa618
Kevin Robles <kevinronu@gmail.com>
2023-09-20 01:29:34 -0500

    chore: update ResultsStore

--- client/src/stores/SearchResultStore.ts
+++ client/src/stores/SearchResultStore.ts
@@ -2,17 +2,23 @@ import { defineStore } from "pinia";
 
 import { SearchResult } from "../models/document.model";
 
+function initializeSearchResult(): SearchResult {
+  const storedSearchResult = JSON.parse(
+    localStorage.getItem("searchResult") || "{}"
+  );
+  if (Object.keys(storedSearchResult).length === 0) {
+    return { total_found: 0, documents: [] };
+  }
+  return storedSearchResult;
+}
+
 export const useSearchResultStore = defineStore("searchResultStore", {
   state: () => ({
-    searchResult: JSON.parse(
-      localStorage.getItem("searchResult") || "{}"
-    ) as SearchResult,
+    searchResult: initializeSearchResult(),
   }),
   getters: {
     getDocumentById(state) {
       const documents = state.searchResult.documents;
-      // if (!documents) return;
-      // console.log("documents", documents);
       return (id: string) => documents.find((document) => document.id === id);
     },
     getDocuments(state) {
@@ -21,14 +27,21 @@ export const useSearchResultStore = defineStore("searchResultStore", {
     getTotal(state) {
       return state.searchResult.total_found;
     },
-    checkIfEmpty(state) {
-      return Object.keys(state.searchResult).length === 0;
+    checkIfHaveDocuments(state) {
+      return state.searchResult.documents.length != 0;
     },
   },
   actions: {
     updateSearchResult(searchResult: SearchResult) {
       this.searchResult = searchResult;
       localStorage.setItem("searchResult", JSON.stringify(searchResult));
+    },
+    clearSearchResult() {
+      this.searchResult = {
+        total_found: 0,
+        documents: [],
+      };
+      localStorage.setItem("searchResult", JSON.stringify(this.searchResult));
     },
   },
 });
