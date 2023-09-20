3a60e16e5545adf027bbf00e95405b08441fb67a
Kevin Robles <kevinronu@gmail.com>
2023-09-20 01:28:30 -0500

    chore: add NoResultsComponent

---
+++ client/src/components/NoResultsComponent.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import NoResultsIcon from "./icons/NoResultsIcon.vue";
+</script>
+
+<template>
+  <div class="flex-auto flex flex-col justify-center items-center">
+    <NoResultsIcon class="w-[12.5rem] h-[12.5rem] mb-2" />
+    <p class="text-xl font-semibold">No results found</p>
+  </div>
+</template>
