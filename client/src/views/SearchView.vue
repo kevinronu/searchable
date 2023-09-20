71b1e506ec2b03fb8ddb7171c99646c3058ba75f
Kevin Robles <kevinronu@gmail.com>
2023-09-20 01:30:22 -0500

    chore: add filters

--- client/src/views/SearchView.vue
+++ client/src/views/SearchView.vue
@@ -1,23 +1,61 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { watch, onMounted } from "vue";
 import { useRoute, onBeforeRouteUpdate } from "vue-router";
 
+import FiltersComponent from "../components/FiltersComponent.vue";
+import NoResultsComponent from "../components/NoResultsComponent.vue";
 import DocumentPreviewComponent from "../components/DocumentPreviewComponent.vue";
 import PaginationComponent from "../components/PaginationComponent.vue";
 import { useSearchResultStore } from "../stores/SearchResultStore";
 import { searchDocument } from "../services/documents-service";
 import { SearchResult } from "../models/document.model.ts";
+import { useFiltersStore } from "../stores/FiltersStore";
+
 const route = useRoute();
 
 const searchResult = useSearchResultStore();
+const filters = useFiltersStore();
 
-onMounted(() => {
-  if (route.params.query != "") {
-    searchDocument(String(route.params.query), Number(route.params.currentPage))
+watch(
+  [() => filters.getSort, () => filters.getFrom, () => filters.getTo],
+  ([newSort, newFrom, newTo]) => {
+    searchDocument(
+      String(route.params.query),
+      Number(route.params.currentPage),
+      {
+        sort: newSort,
+        from: newFrom,
+        to: newTo,
+      }
+    )
       .then((data: SearchResult) => {
         searchResult.updateSearchResult(data);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        searchResult.clearSearchResult();
+        console.log(error);
+      });
+  }
+);
+
+onMounted(() => {
+  if (route.params.query != "") {
+    searchDocument(
+      String(route.params.query),
+      Number(route.params.currentPage),
+      {
+        sort: filters.getSort,
+        from: filters.getFrom,
+        to: filters.getTo,
+      }
+    )
+      .then((data: SearchResult) => {
+        searchResult.updateSearchResult(data);
+      })
+      .catch((error) => {
+        searchResult.clearSearchResult();
+        console.log(error);
+      });
   }
 });
 
@@ -26,21 +64,30 @@ onBeforeRouteUpdate(async (to, from) => {
     to.params.query !== from.params.query ||
     to.params.currentPage !== from.params.currentPage
   ) {
-    searchDocument(String(to.params.query), Number(to.params.currentPage))
+    searchDocument(String(to.params.query), Number(to.params.currentPage), {
+      sort: filters.getSort,
+      from: filters.getFrom,
+      to: filters.getTo,
+    })
       .then((data: SearchResult) => {
         searchResult.updateSearchResult(data);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        searchResult.clearSearchResult();
+        console.log(error);
+      });
   }
 });
 </script>
 <template>
-  <main>
+  <main class="flex-auto flex flex-col justify-between">
+    <FiltersComponent />
     <div
       class="container m-auto grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-4 p-4"
+      v-if="searchResult.checkIfHaveDocuments"
     >
       <router-link
-        v-for="document in searchResult?.getDocuments"
+        v-for="document in searchResult.getDocuments"
         :key="document.id"
         :to="`/document/${document.id}`"
         class="focus:outline-none focus:ring focus:ring-blue-400 focus:rounded"
@@ -55,13 +102,23 @@ onBeforeRouteUpdate(async (to, from) => {
         />
       </router-link>
     </div>
+    <NoResultsComponent v-if="!searchResult.checkIfHaveDocuments" />
     <PaginationComponent
-      v-if="searchResult"
+      v-if="searchResult.checkIfHaveDocuments"
       :location="`/search/${String(route.params.query)}`"
       :current-page="Number(route.params.currentPage)"
       :total-quantity="searchResult.getTotal"
       :quantity-per-page="20"
       :max-pages-generated="10"
     />
+    <div v-if="!searchResult.checkIfHaveDocuments" class="text-center">
+      <router-link
+        :to="`/search/${String(route.params.query)}/page/1`"
+        class="cursor-pointer py-1 px-1 rounded"
+        active-class="border-2 border-pink-600 dark:border-pink-600 bg-pink-200 dark:bg-pink-900"
+      >
+        1
+      </router-link>
+    </div>
   </main>
 </template>
